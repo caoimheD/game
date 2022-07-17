abc4d6b46a627b8572e92ad7ed189dd08eec0374
caoimheD <caoimhe.db5@gmail.com>
2022-07-17 17:58:45 +0000

    add functions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,64 @@
 /**
+ * Define variables
+ *  
+ */
+
+ let playerScore = document.getElementById("user-score");
+ let computerScore = document.getElementById("comp-score");
+ let reset = document.getElementById("reset");
+
+/**
  * Add event listener to buttons
- *   button.addEventListener("click", function (){
-        let playerChoice = this.getAttribute("data-choice");
-        playGame(playerChoice);
+ *  
  */
 
 let button = document.getElementsByClassName('choicebtn');
 for (let i = 0; i < button.length; i++) {
- button[i].addEventListener("click", runGame);
+ button[i].addEventListener("click", function (){
+    let playerChoice = this.getAttribute("data-choice");
+    runGame(playerChoice);
+});
+}
+ 
+function runGame(playerChoice) {
+    let choices = ["rock", "paper", "scissors"];
+
+    let computerChoice = Math.floor(Math.random() * 3);
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
 }
 
-
-function runGame() {
+function checkWinner () {
+    if (playerChoice == computerChoice) {
+        return draw;
+    }  else if (playerChoice === "rock" && computerChoice === "scissors") {
+        return playerWin;
     
-    alert("hello");
-    console.log("hello");
+      } else if (playerChoice === "paper" && computerSelection === "rock") {
+        return playerWin;
+    
+      } else if (playerChoice === "scissors" && computerSelection === "paper") {
+        return playerWin;
+    
+      } else {
+        return computerWin;
+      }
 }
 
+function playerWin () {
+    alert("you won!");
+    playerScore++;
+}
+function computerWin () {
+    alert("you lost!");
+    computerScore++;
+}
+function draw () {
+    alert("it's a draw!");
+    computerScore++;
+    playerScore++;
+
+}
+/**
+ * Results
+ */
