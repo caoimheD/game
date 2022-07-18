40577975816fee6baff212051836ee8a88f01f8e
caoimheD <caoimhe.db5@gmail.com>
2022-07-18 18:18:01 +0000

    add new variables and functions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,77 +2,99 @@
  * Define variables
  *  
  */
+const compChoiceText = document.getElementById("comp-choice");
+const userChoiceText = document.getElementById("user-choice");
+const resultText = document.getElementById("result");
 
- let playerScore = document.getElementById("user-score");
- let computerScore = document.getElementById("comp-score");
- let reset = document.getElementById("reset");
+let playerScore = document.getElementById("user-score");
+let computerScore = document.getElementById("comp-score");
+let reset = document.getElementById("reset");
+let playerChoice;
+let computerChoice;
+let result;
+/**
+ * add event listener to buttons and display user/computer choices in text
+ *  
+ */
+const button = document.getElementsByClassName("choicebtn");
+for (let i = 0; i < button.length; i++) {
+ button[i].addEventListener("click", (e) => {
+  playerChoice = e.target.id;
+  userChoiceText.innerHTML = playerChoice;
+  compChoice();
+  checkWinner();
+ }
+ )};
 
 /**
  * function that runs the game
  *  
  */
-function runGame(playerChoice) {
+function runGame(e) {
  
-    let button = document.getElementsByClassName('choicebtn');
-    for (let i = 0; i < button.length; i++) {
-    let playerChoice = this.getAttribute("data-choice");
+   playerChoice = e.target.id;
+   userChoiceText.innerHTML = playerChoice;
+    compChoice ();
+    checkWinner();
 
   }
-}
+
 /**
  * function that determines computer choice
  *  
  */
 function compChoice () {
 
-    let computerChoice = Math.floor(Math.random() * 3);
+  const randomOption = Math.floor(Math.random() * 3);
 
-    switch(computerChoice) {
-      case 0:
-          choice = "rock";
-          break;
-      case 1:
-          choice = "paper";
-          break;
-      case 2:
-          choice = "scissors";
-          break; 
-  }
-  return choice;
+   if (randomOption === 1) {
+    computerChoice = 'rock'
+   }
+   if (randomOption === 2) {
+    computerChoice = 'paper'
+   }
+   if (randomOption === 1) {
+    computerChoice = 'scissors'
+   }
+   compChoiceText.innerHTML = computerChoice;   
+
 }
 
+/**
+ * Checks the winner of the game
+ */
 
 function checkWinner () {
     if (playerChoice == computerChoice) {
-        return draw;
-    }  else if (playerChoice === "rock" && computerChoice === "scissors") {
-        return playerWin;
-    
-      } else if (playerChoice === "paper" && computerSelection === "rock") {
-        return playerWin;
-    
-      } else if (playerChoice === "scissors" && computerSelection === "paper") {
-        return playerWin;
-    
-      } else {
-        return computerWin;
-      }
+      result = 'its a draw!';
 }
-
-function playerWin () {
-    alert("you won!");
-    playerScore++;
+if (computerChoice === 'rock' && playerChoice === 'paper') {
+  result = 'you won!';
 }
-function computerWin () {
-    alert("you lost!");
-    computerScore++;
+if (computerChoice === 'rock' && playerChoice === 'scissors') {
+  result = 'you lost!';
 }
-function draw () {
-    alert("it's a draw!");
-    computerScore++;
-    playerScore++;
+if (computerChoice === 'paper' && playerChoice === 'scissors') {
+  result = 'you won!';
+}
+if (computerChoice === 'paper' && playerChoice === 'rock') {
+  result = 'you lost!';
+}
+if (computerChoice === 'scissors' && playerChoice === 'rock') {
+  result = 'you won!';
+}
+if (computerChoice === 'scissors' && playerChoice === 'paper') {
+  result = 'you lost!';
+}
+resultText.innerHTML = result;
 
 }
+
 /**
- * Results
+ * Scores
+ */
+
+
+/**
+ * Reset game
  */
