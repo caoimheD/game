b15dfaefb24c9878060d51d0eb2142c895561023
caoimheD <caoimhe.db5@gmail.com>
2022-07-25 20:12:21 +0000

    edit computer choice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,16 +26,16 @@ let score;
  */
 const button = document.getElementsByClassName("choicebtn");
 for (let i = 0; i < button.length; i++) {
- button[i].addEventListener("click", (e) => {
-  playerChoice = e.target.id;
-  userChoiceText.innerHTML = playerChoice;
-  compChoice();
-  checkWinner();
-  checkScore();
-  gameRound();
-  changeImg();
- }
- )};
+  button[i].addEventListener("click", (e) => {
+    playerChoice = e.target.id;
+    userChoiceText.innerHTML = playerChoice;
+    compChoice();
+    checkWinner();
+    checkScore();
+    gameRound();
+    changeImg();
+  })
+};
 
 /**
  * function that runs the game
@@ -55,20 +55,13 @@ for (let i = 0; i < button.length; i++) {
  * function that determines computer choice
  *  
  */
-function compChoice () {
+function compChoice() {
 
   const randomOption = Math.floor(Math.random() * 3);
+  const computerChoices = ['rock', 'paper', 'scissors'];
+  computerChoice = computerChoices[randomOption];
 
-   if (randomOption === 0) {
-    computerChoice = 'rock'
-   }
-   if (randomOption === 1) {
-    computerChoice = 'paper'
-   }
-   if (randomOption === 2) {
-    computerChoice = 'scissors'
-   }
-   compChoiceText.innerHTML = computerChoice;   
+  compChoiceText.innerHTML = computerChoices[randomOption];
 
 }
 
@@ -76,30 +69,30 @@ function compChoice () {
  * Checks the winner of the game
  */
 
-function checkWinner () {
-    if (playerChoice == computerChoice) {
-      result = 'its a draw!';
-      score = 'playerAndcomputer';
-} else if (computerChoice === 'rock' && playerChoice === 'paper') {
-  result = 'you won!';
-  score = 'player';
-} else if (computerChoice === 'rock' && playerChoice === 'scissors') {
-  result = 'you lost!';
-  score = 'computer';
-} else if (computerChoice === 'paper' && playerChoice === 'scissors') {
-  result = 'you won!';
-  score = 'player';
-} else if (computerChoice === 'paper' && playerChoice === 'rock') {
-  result = 'you lost!';
-  score = 'computer';
-} else if (computerChoice === 'scissors' && playerChoice === 'rock') {
-  result = 'you won!';
-  score = 'player';
-} else if (computerChoice === 'scissors' && playerChoice === 'paper') {
-  result = 'you lost!';
-  score = 'computer';
-}
-resultText.innerHTML = result;
+function checkWinner() {
+  if (playerChoice == computerChoice) {
+    result = 'its a draw!';
+    score = 'playerAndcomputer';
+  } else if (computerChoice === 'rock' && playerChoice === 'paper') {
+    result = 'you won!';
+    score = 'player';
+  } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
+    result = 'you lost!';
+    score = 'computer';
+  } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
+    result = 'you won!';
+    score = 'player';
+  } else if (computerChoice === 'paper' && playerChoice === 'rock') {
+    result = 'you lost!';
+    score = 'computer';
+  } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+    result = 'you won!';
+    score = 'player';
+  } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
+    result = 'you lost!';
+    score = 'computer';
+  }
+  resultText.innerHTML = result;
 
 }
 
@@ -107,43 +100,42 @@ resultText.innerHTML = result;
  * Calculates user and computer scores
  */
 
-function checkScore(){
+function checkScore() {
 
-    if (score == 'player') {
-      playerScore++;
-    } else if (score == 'computer') {
-      computerScore++;
-    }
-    else if (score == 'playerAndcomputer') {
-      computerScore++;
-      playerScore++;
-    }
-    playerScoreText.innerHTML = playerScore;
-    compScoreText.innerHTML = computerScore;
+  if (score == 'player') {
+    playerScore++;
+  } else if (score == 'computer') {
+    computerScore++;
+  } else if (score == 'playerAndcomputer') {
+    computerScore++;
+    playerScore++;
   }
+  playerScoreText.innerHTML = playerScore;
+  compScoreText.innerHTML = computerScore;
+}
 /**
  * Resets the game (scores, images and text).
  */
 
 function gameReset() {
-    computerScore = 0;
-    playerScore = 0;
-    result = "";
-    playerChoice = 'Make a choice!';
-    computerChoice = 'Make a choice!';
+  computerScore = 0;
+  playerScore = 0;
+  result = "";
+  playerChoice = 'Make a choice!';
+  computerChoice = 'Make a choice!';
 
-    playerScoreText.innerHTML = playerScore;
-    compScoreText.innerHTML = computerScore;
-    resultText.innerHTML = result;
-    compChoiceText.innerHTML = computerChoice;
-    userChoiceText.innerHTML = playerChoice;
-    userImg.src = "assets/images/placeholder.jpg";
-    compImg.src = "assets/images/placeholder.jpg";
+  playerScoreText.innerHTML = playerScore;
+  compScoreText.innerHTML = computerScore;
+  resultText.innerHTML = result;
+  compChoiceText.innerHTML = computerChoice;
+  userChoiceText.innerHTML = playerChoice;
+  userImg.src = "assets/images/placeholder.jpg";
+  compImg.src = "assets/images/placeholder.jpg";
 
-    rockBtn.disabled = false;
-    paperBtn.disabled = false;
-    scissorsBtn.disabled = false;
-  
+  rockBtn.disabled = false;
+  paperBtn.disabled = false;
+  scissorsBtn.disabled = false;
+
 }
 
 /**
@@ -154,23 +146,23 @@ function gameRound() {
 
   if (playerScore === 5) {
     result = 'you won the round! click reset to play again!';
-    gameEnd(); 
-  } else if (computerScore === 5 ) {
+    gameEnd();
+  } else if (computerScore === 5) {
     result = 'computer won the round! click reset to play again!';
     gameEnd();
-}
-resultText.innerHTML = result;
+  }
+  resultText.innerHTML = result;
 }
 /**
  * function that ends the game and disables buttons from being clicked
  */
 
- function gameEnd() {
-    setTimeout(gameReset, 10000);
-    rockBtn.disabled = true;
-    paperBtn.disabled = true;
-    scissorsBtn.disabled = true;
- }
+function gameEnd() {
+  setTimeout(gameReset, 10000);
+  rockBtn.disabled = true;
+  paperBtn.disabled = true;
+  scissorsBtn.disabled = true;
+}
 
 /**
  * Changes the image based on user choice and computer choice
@@ -178,18 +170,18 @@ resultText.innerHTML = result;
 
 function changeImg() {
   if (playerChoice === 'rock') {
-    userImg.src="assets/images/rockanimated.jpg";
+    userImg.src = "assets/images/rockanimated.jpg";
   } else if (playerChoice === 'scissors') {
-    userImg.src="assets/images/scissorsanimated.jpg";
+    userImg.src = "assets/images/scissorsanimated.jpg";
   } else if (playerChoice === 'paper') {
-    userImg.src="assets/images/paperanimated.jpg";
-  } 
+    userImg.src = "assets/images/paperanimated.jpg";
+  }
 
   if (computerChoice === 'rock') {
-    compImg.src="assets/images/rockanimated.jpg";
+    compImg.src = "assets/images/rockanimated.jpg";
   } else if (computerChoice === 'scissors') {
-    compImg.src="assets/images/scissorsanimated.jpg";
+    compImg.src = "assets/images/scissorsanimated.jpg";
   } else if (computerChoice === 'paper') {
-    compImg.src="assets/images/paperanimated.jpg";
-  } 
+    compImg.src = "assets/images/paperanimated.jpg";
+  }
 }
